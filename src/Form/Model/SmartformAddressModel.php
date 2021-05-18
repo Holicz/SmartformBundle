96d61d70e80145c03bd46fbd830629a166e41f9a
Michal Doležal <michal.dolezal@gmail.com>
2021-05-18 10:52:23 +0200

    fix model error when address is not chosen

--- src/Form/Model/SmartformAddressModel.php
+++ src/Form/Model/SmartformAddressModel.php
@@ -15,19 +15,19 @@ final class SmartformAddressModel
      * @Assert\NotBlank(message="Kód z RÚIAN databáze je povinný")
      * @Assert\Length(max="255", maxMessage="Kód z RÚIAN databáze může mít maximálně {{ limit }} znaků")
      */
-    public string $code;
+    public ?string $code = null;
 
     /**
      * @Assert\NotBlank(message="Zeměpisná šířka je povinná")
      * @Assert\Length(max="255", maxMessage="Zeměpisná šířka může mít maximálně {{ limit }} znaků")
      */
-    public string $latitude;
+    public ?string $latitude = null;
 
     /**
      * @Assert\NotBlank(message="Zeměpisná délka je povinná")
      * @Assert\Length(max="255", maxMessage="Zeměpisná délka může mít maximálně {{ limit }} znaků")
      */
-    public string $longitude;
+    public ?string $longitude = null;
 
     /**
      * @Assert\Expression("this.street or this.cityPart", message="Ulice je povinná")
@@ -40,13 +40,13 @@ final class SmartformAddressModel
      *
      * @Assert\Length(max="255", maxMessage="Ulice může mít maximálně {{ limit }} znaků")
      */
-    public string $cityPart;
+    public ?string $cityPart = null;
 
     /**
      * @Assert\NotBlank(message="Číslo popisné je povinné")
      * @Assert\Length(max="255", maxMessage="Číslo popisné může mít maximálně {{ limit }} znaků")
      */
-    public string $houseNumber;
+    public ?string $houseNumber = null;
 
     /**
      * @Assert\Length(max="255", maxMessage="Číslo orientační může mít maximálně {{ limit }} znaků")
@@ -57,31 +57,31 @@ final class SmartformAddressModel
      * @Assert\NotBlank(message="Město je povinné")
      * @Assert\Length(max="255", maxMessage="Město může mít maximálně {{ limit }} znaků")
      */
-    public string $city;
+    public ?string $city = null;
 
     /**
      * @Assert\NotBlank(message="PSČ je povinné")
      * @Assert\Regex("/^(\d{3})\s*(\d{2})$/", message="PSČ není ve správném formátu (např. 110 00)")
      */
-    public string $zipCode;
+    public ?string $zipCode = null;
 
     /**
      * @Assert\NotBlank(message="Okres je povinný")
      * @Assert\Length(max="255", maxMessage="Okres může mít maximálně {{ limit }} znaků")
      */
-    public string $district;
+    public ?string $district = null;
 
     /**
      * @Assert\NotBlank(message="Kraj je povinný")
      * @Assert\Length(max="255", maxMessage="Kraj může mít maximálně {{ limit }} znaků")
      */
-    public string $region;
+    public ?string $region = null;
 
     /**
      * @Assert\NotBlank(message="Země je povinná")
      * @Assert\Length(max="255", maxMessage="Země může mít maximálně {{ limit }} znaků")
      */
-    public string $country;
+    public ?string $country = null;
 
     public static function createEmpty(): self
     {
